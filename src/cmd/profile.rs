d21ac60244146ff6c3383bf12f85b7fbbc08e03e
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2023-08-03 21:49:05 -0400

    Rating system done #11

--- src/cmd/profile.rs
+++ src/cmd/profile.rs
@@ -1,6 +1,6 @@
 use crate::{db::{query, query_value, dissolve}, AppData};
 use super::{signup::Account, jobs::Job};
-use actix_web::{get, post, Responder, web::{Data, Form, self}, HttpResponse};
+use actix_web::{get, post, Responder, web::{Data, Form, self}, HttpResponse, HttpResponseBuilder};
 struct Username{
     username: String,
 }
@@ -33,35 +33,39 @@ pub struct RatingData{
     username: String,
 }
 
-#[get("/rate/{username}")]
+#[post("/rate/{username}")]
 async fn rate(rating_data: Form<RatingData>, data: web::Data<AppData>, username: web::Path<String>) -> impl Responder{
     let review = rating_data.0;
-    let stars = review.stars.clamp(0., 5.);
+    let mut sum = review.stars.clamp(0., 5.);
 
 
     let mut db = data.db.lock().await;
     let res2 = query_value(&mut db, "SELECT page.reviews.stars FROM accounts WHERE username = type::string($username);", Some(("username", username.as_str()))).await.unwrap();
     let res1 = res2.get(0).unwrap();
     let result = res1.as_ref().unwrap();
-    
-
-    let rating = todo!();
+    let len = result.len();
+    if len != 1{
+        //error but should not happen yada yada you know the drill
+        return HttpResponse::BadRequest();
+    }
+    let res = result.get(0).unwrap();
+    let stars = res.get("page").unwrap().get("reviews").unwrap().get("stars").unwrap().as_array().unwrap();
+    // let mut sum = s;
+    let div = stars.len() + 1;
+    for i in stars{
+        let j = i.as_f64().unwrap() as f32;
+        sum += j;
+    }
+    let new_avg = sum / div as f32;
 
     let q = "UPDATE accounts
     SET 
     page.avg_rating = $rating,
     page.reviews += $review,
     WHERE username = type::string($username);";
-    crate::db::dissolve(query::<Account>(&mut db, q, Some((("rating", "review", "username"), (rating, review, username.as_str())))).await, 34);
+    crate::db::dissolve(query::<Account>(&mut db, q, Some((("rating", "review", "username"), (new_avg, review, username.as_str())))).await, 34);
 
-
-    
-
-
-
-
-    todo!() as HttpResponse
-    // HttpResponse::Ok().body()
+    HttpResponse::Ok()
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -98,6 +102,6 @@ async fn settings_post(setting: Form<SettingsData>, data: Data<AppData>) -> impl
     let mut db = data.db.lock().await;
     dissolve(query_value(&mut db, surrealql, Some(setting.0)).await, 44);
     //might get a runtime error bcs of surrealql since password field is unused?
-    
+
     HttpResponse::Ok()
 }
