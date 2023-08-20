055713389b8c248a7ffab3aea71dca3b8322cc70
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2023-08-19 23:43:13 -0400

    Phind is crapp

--- src/cmd/chats.rs
+++ src/cmd/chats.rs
@@ -1,80 +1,47 @@
-//Goodbye WebSockets. Hello XMTPPPSPSPPSPSPSSPPPP or whatever
-//XMPPS (XMPP + TLS)
+use tokio::net::TcpStream;
+use tokio_xmpp::xmpp_stream::XMPPStream;
+use quick_xml::Writer;
+use quick_xml::events::{Event, BytesStart, BytesEnd};
+use quick_xml::events::attributes::Attribute;
+use std::str::from_utf8;
 
-use actix_session::Session;
-use actix_web::{web, Responder, get, HttpResponse};
+async fn send_message(xmpp_stream: &mut XMPPStream<TcpStream>, jid: &str, message: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut writer = Writer::new(Vec::new());
+    writer.write_event(Event::Start(BytesStart::borrowed_name(b"message")))?;
+    writer.write_event(Event::Start(BytesStart::borrowed(b"body", "body".len())))?;
+    writer.write_event(Event::Text(BytesText::from_plain_str(message)))?;
+    writer.write_event(Event::End(BytesEnd::borrowed(b"body")))?;
+    writer.write_event(Event::End(BytesEnd::borrowed(b"message")))?;
 
-#[get("/chat/{room}")]
-async fn chat(room: web::Path<String>, session: Session) -> impl Responder {
-    HttpResponse::Ok().body("Sent XMPP message: {message} to {recipient}!")
+    let message_stanza = from_utf8(writer.inner())?;
+    xmpp_stream.send_stanza(message_stanza).await?;
+
+    Ok(())
 }
 
+async fn receive_message(xmpp_stream: &mut XMPPStream<TcpStream>) -> Result<String, Box<dyn std::error::Error>> {
+    loop {
+        let stanza = xmpp_stream.next_stanza().await?;
+        let reader = Reader::from_str(&stanza);
+        let mut buf = Vec::new();
 
-
-
-// use std::error::Error;
-// use std::str::FromStr;
-// use tokio::stream::StreamExt;
-// use tokio_xmpp::{Client, Jid, Message, MessageType, Stanza};
-
-
-
-
-// async fn send_message(client: &Client, recipient: &str, message: &str) -> Result<(), Box<dyn Error>> {
-//     let message = Message::new(
-//         Some(Jid::from_str(recipient)?),
-//         Some(Jid::from_str("your_jid")?),
-//         MessageType::Chat,
-//         message.to_string(),
-//         None,
-//     );
-//     client.send(message).await?;
-//     Ok(())
-// }
-
-
-// async fn receive_message(client: &Client) -> Result<(), Box<dyn Error>> {
-//     let mut stream = client.stream().await?;
-//     while let Some(stanza) = stream.next().await {
-//         match stanza? {
-//             Stanza::Message(message) => {
-//                 println!("Received message: {:?}", message);
-//             }
-//             _ => (),
-//         }
-//     }
-//     Ok(())
-// }
-
-
-
-
-
-
-
-
-// use tokio::net::TcpStream;
-// use tokio_xmpp::xmpp_stream::XMPPStream;
-// use quick_xml::Writer;
-// use quick_xml::events::{Event, BytesStart, BytesEnd};
-// use quick_xml::events::attributes::Attribute;
-// use std::str::from_utf8;
-
-// async fn send_message(jid: &str, message: &str) -> Result<(), Box<dyn std::error::Error>> {
-//     let stream = TcpStream::connect("your_xmpp_server:5222").await?;
-//     let mut xmpp_stream = XMPPStream::new(stream);
-
-//     let mut writer = Writer::new(Vec::new());
-//     writer.write_event(Event::Start(BytesStart::borrowed_name(b"message")))?;
-//     writer.write_event(Event::Start(BytesStart::borrowed(b"body", "body".len())))?;
-//     writer.write_event(Event::Text(BytesText::from_plain_str(message)))?;
-//     writer.write_event(Event::End(BytesEnd::borrowed(b"body")))?;
-//     writer.write_event(Event::End(BytesEnd::borrowed(b"message")))?;
-
-//     let message_stanza = from_utf8(writer.inner())?;
-//     xmpp_stream.send_stanza(message_stanza).await?;
-
-//     Ok(())
-// }
-
-
+        for e in reader {
+            match e? {
+                Event::Start(ref e) if e.name() == b"message" => {
+                    // We found a message stanza, now we need to look for the body
+                    for e in reader {
+                        match e? {
+                            Event::Start(ref e) if e.name() == b"body" => {
+                                // We found the body, let's return its text
+                                let text = reader.read_text(b"body", &mut buf)?;
+                                return Ok(text);
+                            },
+                            _ => {},
+                        }
+                    }
+                },
+                _ => {},
+            }
+        }
+    }
+}
