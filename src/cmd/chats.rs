e61bfdeb248eafdac1c490c4a6bb87a6033e3cb5
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2023-08-21 20:19:03 -0400

    Note addition

--- src/cmd/chats.rs
+++ src/cmd/chats.rs
@@ -2,3 +2,5 @@
 //Rustls
 //XMPP(S) transfer protocol for messages? Tokio!
 //Basically anything that can send and receive messages
+
+//Note: we added Rippedplushie who now has the codebase as of the previous commit.
