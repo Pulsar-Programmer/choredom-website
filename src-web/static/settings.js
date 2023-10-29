e60c30baeb4411680725402fc22c0aa075f24e77
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2023-10-28 23:15:43 -0400

    monkie ? #111 ?

--- src-web/static/settings.js
+++ src-web/static/settings.js
@@ -1,29 +1,26 @@
-let url = 'http://localhost:8080/settings-post';
-let data = {
-    username: 'username', 
-    password: 'password',
-    displayname: 'displayname',
-    location: 'location',
-    bio: 'bio',
-    pfp: 'pfp'
-};
-fetch(url, {
-    method: 'POST', 
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data), 
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch((error) => {
-  console.error('Error:', error);
-});
-
-
-
-
-
+function settings_post(){
+    let url = 'http://localhost:8080/settings-post';
+    let data = {
+        username: 'username', 
+        password: 'password',
+        displayname: 'displayname',
+        location: 'location',
+        bio: 'bio',
+        pfp: 'pfp'
+    };
+    fetch(url, {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), 
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => {
+    console.error('Error:', error);
+    });
+}
 
 
 
