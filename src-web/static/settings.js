e0cf95800d12461c18d0013abe1a30141c1e437e
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2023-10-29 15:20:23 -0400

    #128 yeet
    Co-authored-by: Shatmaster <Lockroach@users.noreply.github.com>

--- src-web/static/settings.js
+++ src-web/static/settings.js
@@ -22,10 +22,6 @@ function settings_post(){
     });
 }
 
-
-
-
-
 window.addEventListener("load", function() {
     // const submitButton = form.querySelector('input[type="submit"]');
 // submitButton.addEventListener("click", function(event) {
@@ -33,22 +29,20 @@ window.addEventListener("load", function() {
 // const post_form = document.getElementById("delete-account-form");
 // post_form.addEventListener("submit", function(event) {
     
-
-//     dont let it submit invalid fields(just like in signup or whatever)
-//     if (){
-//         alert("Please don't submit a blank field.")
-//         event.preventDefault();
-//     }
+//     // let username = document.getElementById("username");
+//     // let displayname = document.getElementById("displayname");
+//     // let location = document.getElementById("location");
+//     // let email = document.getElementById("email");
+//     // let bio = document.getElementById("bio");
+//    // dont let it submit invalid fields(just like in signup or whatever)
+//     // if (username === "" || displayname === "" || location === "" || email === "" || bio === ""){
+//     //     alert("Please don't submit a blank field.")
+//     //     event.preventDefault();
+//     // } 
 
 //     post_form.submit();
 
 // });
-
-
-
-
-
-
 
 const delete_form = document.getElementById("delete-account-form");
 post_form.addEventListener("submit", function(event) {
@@ -67,11 +61,3 @@ post_form.addEventListener("submit", function(event) {
 
 });
 });
-
-
-
-
-    
-    
-    
-    
