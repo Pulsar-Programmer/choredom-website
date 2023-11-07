684b758390b18872dd7f4f94f8cbcc7d125de208
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2023-11-06 21:36:10 -0500

    Back and forth LOL #101
    Co-authored-by: Shatmaster <Lockroach@users.noreply.github.com>

--- src-web/static/jobs.js
+++ src-web/static/jobs.js
@@ -23,19 +23,17 @@ window.onload = function() {
 }
   
 function displayJob(job){
-//jobsData.user.username, jobsData.user.state, jobData.user.rating ..
-//jobsData.data <== anything with to do with the actual job
+    //jobsData.user.username, jobsData.user.state, jobData.user.rating ..
+    //jobsData.data <== anything with to do with the actual job
+    let verification_status = job.user.account_state === "Verified" ? "V" : "Unv";
 
-let verification_status = job.user.state === "Verified" ? "V" : "Unv";
-`
-    <div class="job">
-    <h3>${job.data.title}</h3>
-    <h4><a href="/users/${job.user.username}">${job.user.displayname}</a> (${job.user.username}) (${verification_status}erified User)</h4>
-    <p>${job.data.body}</p>
-    <p>Date of Task: ${job.data.time}</p>
-    <p>Price: $${job.data.price / 100.0}</p>
-    <a href="/${job.id}">Visit Job Post</a>
-    </div>
-`;
-//Shatmaster work on this
+    document.getElementById("post-title").value = job.data.title;
+    document.getElementById("post-location").value = job.data.location;
+    document.getElementById("post-date").value = job.data.time;
+    document.getElementById("post-price").value = job.data.price / 100.0;
+    document.getElementById("post-displayname").value = `<a href="/users/${job.user.username}">${job.user.displayname}</a> (${job.user.username}) (${verification_status}erified User)`;
+    document.getElementById("post-body").value = job.data.body;
+
+
+    //Shatmaster work on this
 }
