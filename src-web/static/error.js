d9bd0b3cecdd973625bda87f4476d1979d28346e
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2023-12-27 22:59:12 -0500

    custom levels within mario wonder

--- src-web/static/error.js
+++ src-web/static/error.js
@@ -1,6 +1,11 @@
 async function handle(response) {
     const answer = await response.json();
     if(response.status === 303){
+        //we are going to have to
+        //check if the response is SeeOther somehow
+        //and if it is then we just go to the place
+        //in the JSON sent maybe instead of using headers
+        //I mean we can idk whatever works. 
         window.location.href = response.headers.get('Location');
         window.location.reload();
     }
