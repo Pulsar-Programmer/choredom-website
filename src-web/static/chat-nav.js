c2e355db6ed7ad87b72a11133c3ea7d8627a6ba0
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-03-23 21:39:04 -0400

    Minor work on Chat-Nav

--- src-web/static/chat-nav.js
+++ src-web/static/chat-nav.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function() {
         let dat = Array.from(data);
         if(dat.length == 0) {
             let links_div = document.getElementById("chatList");
-            links_div.innerHTML = `<label>You have no chats as of now!</label>`;
+            links_div.innerHTML += `<p class="stdelem">You have no chats as of now!</p>`;
         }
         dat.forEach(part => add_to_html(part.room_name));
     })
@@ -22,7 +22,7 @@ window.addEventListener("load", function() {
 
 function add_to_html(yapper){
     let url = `/chats/${yapper}`;
-    let html = `<div class="link"><a href=${url}>${yapper}</a></div>`;
+    let html = `<div class="stdelem"><a href=${url}>${yapper}</a></div>`;
     let links_div = document.getElementById("chatList");
     links_div.innerHTML += html;
 }
